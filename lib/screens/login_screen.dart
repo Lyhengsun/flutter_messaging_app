bd18d4a9288b530ed961b60484e1e3a5f9aa721d
Lyhengsun <101932930+Lyhengsun@users.noreply.github.com>
2024-05-14 23:10:54 +0700

    remove unnecessary import in login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_messaging_app/components/my_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
