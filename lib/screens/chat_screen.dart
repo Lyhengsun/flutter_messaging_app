f89359fe05462f9dd060d306e4376c62baa1089b
Lyhengsun <101932930+Lyhengsun@users.noreply.github.com>
2024-05-17 11:21:39 +0700

    add user list and chat screen

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  final String receiverUserEmail;
+  final String receiverUserID;
+  const ChatScreen({
+    super.key,
+    required this.receiverUserEmail,
+    required this.receiverUserID,
+  });
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.receiverUserEmail),
+      ),
+    );
+  }
+}
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,3 +1,4 @@
 export "package:flutter_messaging_app/screens/login_screen.dart";
 export "package:flutter_messaging_app/screens/register_screen.dart";
 export "package:flutter_messaging_app/screens/home_screen.dart";
+export "package:flutter_messaging_app/screens/chat_screen.dart";
