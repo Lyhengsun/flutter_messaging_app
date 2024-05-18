41e630969877f0d1e439891538ab7d5840fcf653
Lyhengsun <hengsun2.168@gmail.com>
2024-05-19 05:15:34 +0700

    added bubble message, last message in home screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -25,8 +25,11 @@ class _ChatScreenState extends State<ChatScreen> {
   void sendMessage() async {
     // only send message when there is text
     if (_messageController.text.isNotEmpty) {
-      await _chatService.sendMessage(widget.receiverUserID,
-          widget.receiverUserEmail, _messageController.text);
+      await _chatService.sendMessage(
+        widget.receiverUserID,
+        widget.receiverUserEmail,
+        _messageController.text,
+      );
     }
     _messageController.clear();
   }
@@ -50,9 +53,8 @@ class _ChatScreenState extends State<ChatScreen> {
       body: Column(
         children: [
           Expanded(
-              child: Container(
-            color: Colors.red,
-          )),
+            child: _buildMessageList(),
+          ),
           _buildMessageInput(displayScale)
         ],
       ),
@@ -60,14 +62,109 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   // build message list
+  Widget _buildMessageList() {
+    return StreamBuilder(
+      stream: _chatService.getMessage(
+        _firebaseAuth.currentUser!.uid,
+        widget.receiverUserID,
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(child: Text('Error ${snapshot.error}'));
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        var messageDocumentList = snapshot.data!.docs;
+        return ListView(
+          children: () {
+            List<Widget> messageItemList = [];
+
+            // check if the sender is the same. if true show email, else no email
+            String userEmail = "";
+            for (var i = 0; i < messageDocumentList.length; i++) {
+              String newUserEmail = (messageDocumentList[i].data()
+                  as Map<String, dynamic>)["senderEmail"];
+              if (newUserEmail != userEmail) {
+                userEmail = newUserEmail;
+                messageItemList
+                    .add(_buildMessageItem(messageDocumentList[i], false));
+              } else {
+                messageItemList
+                    .add(_buildMessageItem(messageDocumentList[i], true));
+              }
+            }
+            return messageItemList;
+          }(),
+        );
+      },
+    );
+  }
 
   // build message item
-  Widget _buildMessageItem(DocumentSnapshot document) {
+  Widget _buildMessageItem(DocumentSnapshot document, bool sameUser) {
     Map<String, dynamic> data = document.data() as Map<String, dynamic>;
 
     // align message to the right if it is from the current, left if it is the other user
-    var alignment = (data["senderId"] == _firebaseAuth.currentUser!.uid) ? Alignment.centerRight : Alignment.centerLeft;
+    bool isCurrentUser = (data["senderId"] == _firebaseAuth.currentUser!.uid);
+    var alignment =
+        isCurrentUser ? Alignment.centerRight : Alignment.centerLeft;
+    var crossAlignment =
+        isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start;
+    Color messageBubbleColor =
+        isCurrentUser ? Colors.lightBlue : Colors.grey.shade300;
+    Color messageTextColor = isCurrentUser ? Colors.white : Colors.black87;
 
+    return Container(
+      alignment: alignment,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.7,
+        child: Column(
+          crossAxisAlignment: crossAlignment,
+          children: [
+            // show the sender name only once for consecutive messages
+            sameUser
+                ? const SizedBox()
+                : Padding(
+                  padding: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+                  child: Text(
+                      data["senderEmail"],
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+              decoration: BoxDecoration(
+                color: messageBubbleColor,
+                borderRadius: BorderRadius.only(
+                  topRight:
+                      isCurrentUser ? Radius.zero : const Radius.circular(10),
+                  bottomRight:
+                      isCurrentUser ? Radius.zero : const Radius.circular(10),
+                  topLeft:
+                      isCurrentUser ? const Radius.circular(10) : Radius.zero,
+                  bottomLeft:
+                      isCurrentUser ? const Radius.circular(10) : Radius.zero,
+                ),
+              ),
+              child: Text(
+                data["message"],
+                style: TextStyle(color: messageTextColor),
+              ),
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   // build message input
@@ -85,6 +182,7 @@ class _ChatScreenState extends State<ChatScreen> {
             width: 10,
           ),
           GestureDetector(
+            onTap: sendMessage,
             child: Container(
               padding: const EdgeInsets.all(15),
               decoration: BoxDecoration(
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_messaging_app/screens/screens.dart';
 import 'package:flutter_messaging_app/services/auth/auth_service.dart';
+import 'package:flutter_messaging_app/services/chat/chat_service.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -23,16 +24,16 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final double displayScale = MediaQuery.of(context).size.height / 900;
-    final double header1Size = 24 * displayScale;
-    final double header2Size = 16 * displayScale;
+    // final double header1Size = 24 * displayScale;
+    // final double header2Size = 16 * displayScale;
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "Home Page",
           style: TextStyle(
             fontWeight: FontWeight.bold,
             color: Colors.white,
-            fontSize: header1Size,
+            fontSize: 20,
           ),
         ),
         backgroundColor: Colors.lightBlue,
@@ -79,19 +80,60 @@ class _HomePageState extends State<HomePage> {
     Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
 
     if (_auth.currentUser!.email != data["email"]) {
-      return ListTile(
-        title: Text(data["email"]),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ChatScreen(
-                receiverUserEmail: data["email"],
-                receiverUserID: data["uid"],
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+        child: ListTile(
+          title: Text(data["email"]),
+          subtitle: StreamBuilder(
+            stream:
+                ChatService().getMessage(_auth.currentUser!.uid, data["uid"]),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text("Error: ${snapshot.error}");
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Text("Loading.....");
+              }
+
+              var messageList = snapshot.data!.docs;
+              if (messageList.isEmpty) {
+                return Text("start your conversation with ${data["email"]}");
+              }
+              var lastMessage = messageList.last.data() as Map<String, dynamic>;
+              return Row(
+                children: [
+                  Text(
+                    "${lastMessage["senderEmail"]}: ",
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      lastMessage["message"],
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+          tileColor: Colors.grey.shade300,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatScreen(
+                  receiverUserEmail: data["email"],
+                  receiverUserID: data["uid"],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       );
     }
     return const SizedBox();
